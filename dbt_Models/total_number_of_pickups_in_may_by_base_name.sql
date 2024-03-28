ca7f6028ed1533a3c06ca4ee84b08fe5d1aceda9
kimiko <kinqsleyy@gmail.com>
2024-03-28 14:42:24 +0000

    t

---
+++ dbt_Models/total_number_of_pickups_in_may_by_base_name.sql
@@ -0,0 +1,9 @@
+-- Calculate the total number of pickups for each base name in the raw_data_janjune_15 table, considering only records where the pickup date is within the month of May.
+
+SELECT base.base_name AS `Dispatching Base Name`,
+       COUNT(*) AS `Number of Pick Ups for Base`
+FROM airbyte_uber_data.raw_data_janjune_15 AS raw
+JOIN airbyte_uber_data.base_num_and_name AS base ON base.base_num = raw.dispatching_base_num
+WHERE EXTRACT(MONTH FROM raw.pickup_date) = 5
+GROUP BY base.base_name
+ORDER BY COUNT(*) DESC
